8438c87764a123e249436384d6dfaa0fb22e2b6b
Armando Alvear <armando.alvear01@outlook.com>
2024-02-24 01:09:02 -0600

    add signin api route and controller function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
-import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   // Get username email and password from request body
@@ -19,6 +21,33 @@ export const signup = async (req, res, next) => {
     res.status(201).json("User created successfully!");
   } catch (error) {
     // Return error message via next
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  // Get email and password from request body
+  const { email, password } = req.body;
+
+  try {
+    // Query database for user email
+    const validUser = await User.findOne({ email });
+    // Check to see if database returned a valid user, otherwise return error
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    // Compare request password with database password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    // Check if password is a match, otherwise return error
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    // Create JSON web token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    // Destructure out the password and set everything else to rest
+    const { password: pass, ...rest } = validUser._doc;
+    // Send result back to client
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, signin } from "../controllers/auth.controller.js";
 
 // Initialize express router
 const router = express.Router();
 
 // Signup post endpoint
 router.post("/signup", signup);
+
+// Signin post endpoint
+router.post("/signin", signin);
 
 export default router;
