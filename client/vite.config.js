9eb2af7f0acd23e471e50f98a77f16dc7217db14
Armando Alvear <armando.alvear01@outlook.com>
2024-02-24 00:08:34 -0600

    remove dotenv

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
-import dotenv from "dotenv";
 import react from "@vitejs/plugin-react-swc";
-
-dotenv.config();
-
-// Get client port from .env file
-const clientPort = process.env.CLIENT_PORT;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: clientPort,
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+    },
   },
 });
