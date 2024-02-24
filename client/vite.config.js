e7a1f32e7b7e8408e9ff4e7d00034d9d3b61b924
Armando Alvear <armando.alvear01@outlook.com>
2024-02-23 18:24:41 -0600

    change vite server port to .env value

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import dotenv from "dotenv";
+import react from "@vitejs/plugin-react-swc";
+
+dotenv.config();
+
+// Get client port from .env file
+const clientPort = process.env.CLIENT_PORT;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: clientPort,
+  },
+});
