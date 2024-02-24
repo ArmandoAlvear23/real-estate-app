a3bf24b2895e1fe48ed7dc2e35144f0200adb563
Armando Alvear <armando.alvear01@outlook.com>
2024-02-23 18:05:17 -0600

    add mongodb database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+// Initialize dotenv
+dotenv.config();
+
+// Connect to MongoDB database
+mongoose
+  .connect(process.env.MONGODB_CONNECTION)
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// Initialize express app
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Get server port from .env file
+const serverPort = process.env.SERVER_PORT;
+
+// Run port and listen on server port
+app.listen(serverPort, () => {
+  console.log(`Server is running on port ${serverPort}...`);
 });
