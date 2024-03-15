3337a065f78e71efd49c116c50438ae857e4a6b2
Armando Alvear <armando.alvear01@outlook.com>
2024-03-15 16:59:59 -0500

    add create listing api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 
 // Initialize dotenv
@@ -38,6 +39,7 @@ app.listen(serverPort, () => {
 // Routers
 app.use("/api/user", userRouter);
 app.use("/api/auth/", authRouter);
+app.use("/api/listing/", listingRouter);
 
 // Comprehensive error middleware
 app.use((err, req, res, next) => {
