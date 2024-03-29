453b094ecdbc6e30a304242a3847d8e5dacf0671
Armando Alvear <armando.alvear01@outlook.com>
2024-03-29 01:14:06 -0500

    fix fetch path to updated URL

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -32,7 +32,7 @@ export default function Listing() {
         try {
           setLoading(true);
           const fetchListing = async () => {
-            const res = await fetch(`/api/listing/${params.listingId}`);
+            const res = await fetch(`/api/listing/get/${params.listingId}`);
             const data = await res.json();
             if (data.success === false) {
               setError(true);
