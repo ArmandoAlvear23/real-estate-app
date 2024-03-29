ea46d92fa588bb00a082ab7d0b65530c23c0f496
Armando Alvear <armando.alvear01@outlook.com>
2024-03-29 01:14:40 -0500

    create listing card component and add it to search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
+import ListingCard from '../components/ListingCard';
 
 export default function Search() {
 
@@ -198,8 +199,19 @@ export default function Search() {
             >search</button>
           </form>
         </div>
-        <div className="">
+        <div className="flex-1">
             <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">Listing results:</h1>
+            <div className="p-7 flex gap-4">
+                {!loading && listings.length === 0 && (
+                    <p className="text-xl text-slate-700">No listings found...</p>
+                )}
+                {loading && (
+                    <p className="text-xl text-slate-700 text-center w-full">Loading...</p>
+                )}
+                {!loading && listings && listings.map((listing) => (
+                    <ListingCard key={listing._id} listing={listing} />
+                ))}
+            </div>
         </div>
     </div>
   )
