b24978624b633bba5a3cd9c9d71c9b6315fe2f54
Armando Alvear <armando.alvear01@outlook.com>
2024-04-02 20:41:14 -0500

    add show more functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -18,6 +18,7 @@ export default function Search() {
 
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
+    const [showMore, setShowMore] = useState(false);
     
     useEffect(() => {
         
@@ -52,9 +53,15 @@ export default function Search() {
 
         const fetchListings = async () => {
             setLoading(true);
+            setShowMore(false);
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
+            if(data.length > 8) {
+                setShowMore(true);
+            } else {
+                setShowMore(false);
+            }
             setListings(data);
             setLoading(false);
         }
@@ -97,7 +104,20 @@ export default function Search() {
         navigate(`/search?${searchQuery}`)
     }
 
-    
+    const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set("startIndex", startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if (data.length < 9) {
+            setShowMore(false);
+        }
+        setListings([...listings, ...data]);
+    }
+
   return (
     <div className="flex flex-col md:flex-row">
         <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
@@ -201,7 +221,7 @@ export default function Search() {
         </div>
         <div className="flex-1">
             <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">Listing results:</h1>
-            <div className="p-7 flex gap-4">
+            <div className="p-7 flex flex-wrap gap-4">
                 {!loading && listings.length === 0 && (
                     <p className="text-xl text-slate-700">No listings found...</p>
                 )}
@@ -211,6 +231,15 @@ export default function Search() {
                 {!loading && listings && listings.map((listing) => (
                     <ListingCard key={listing._id} listing={listing} />
                 ))}
+
+                {showMore && (
+                    <button
+                        onClick={() => {
+                            onShowMoreClick();
+                        }}
+                        className="text-green-700 hover:underline p-7 w-full text-center"
+                    >Show more</button>
+                )}
             </div>
         </div>
     </div>
